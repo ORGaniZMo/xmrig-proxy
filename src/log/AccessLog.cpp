9b71261b9057c4424712bc117f63eb699917ab46
ORGaniZMo <organizm.nizmo@gmail.com>
2019-04-16 17:12:24 +0500

    2.14.1

--- src/log/AccessLog.cpp
+++ src/log/AccessLog.cpp
@@ -4,8 +4,9 @@
  * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2016-2018 XMRig       <support@xmrig.com>
- *
+ * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -39,7 +40,7 @@
 #include "proxy/Stats.h"
 
 
-AccessLog::AccessLog(xmrig::Controller *controller) :
+xmrig::AccessLog::AccessLog(Controller *controller) :
     m_file(-1)
 {
     const char *fileName = controller->config()->accessLog();
@@ -53,12 +54,12 @@ AccessLog::AccessLog(xmrig::Controller *controller) :
 }
 
 
-AccessLog::~AccessLog()
+xmrig::AccessLog::~AccessLog()
 {
 }
 
 
-void AccessLog::onEvent(IEvent *event)
+void xmrig::AccessLog::onEvent(IEvent *event)
 {
     if (m_file < 0) {
         return;
@@ -68,7 +69,7 @@ void AccessLog::onEvent(IEvent *event)
     {
     case IEvent::LoginType: {
             auto e = static_cast<LoginEvent*>(event);
-            write("#%" PRId64 " login: %s, \"%s\", ua: \"%s\", count: %" PRIu64, e->miner()->id(), e->miner()->ip(), e->miner()->user(), e->miner()->agent(), Counters::miners());
+            write("#%" PRId64 " login: %s, \"%s\", ua: \"%s\", count: %" PRIu64, e->miner()->id(), e->miner()->ip(), e->miner()->user().data(), e->miner()->agent().data(), Counters::miners());
         }
         break;
 
@@ -81,7 +82,7 @@ void AccessLog::onEvent(IEvent *event)
             const double time = (double)(xmrig::currentMSecsSinceEpoch() - e->miner()->timestamp()) / 1000;
 
             write("#%" PRId64 " close: %s, \"%s\", time: %03.1fs, rx/tx: %" PRIu64 "/%" PRIu64 ", count: %" PRIu64,
-                  e->miner()->id(), e->miner()->ip(), e->miner()->user(), time, e->miner()->rx(), e->miner()->tx(), Counters::miners());
+                  e->miner()->id(), e->miner()->ip(), e->miner()->user().data(), time, e->miner()->rx(), e->miner()->tx(), Counters::miners());
         }
         break;
 
@@ -91,12 +92,12 @@ void AccessLog::onEvent(IEvent *event)
 }
 
 
-void AccessLog::onRejectedEvent(IEvent *event)
+void xmrig::AccessLog::onRejectedEvent(IEvent *event)
 {
 }
 
 
-void AccessLog::onWrite(uv_fs_t *req)
+void xmrig::AccessLog::onWrite(uv_fs_t *req)
 {
     delete [] static_cast<char *>(req->data);
 
@@ -105,7 +106,7 @@ void AccessLog::onWrite(uv_fs_t *req)
 }
 
 
-void AccessLog::write(const char *fmt, ...)
+void xmrig::AccessLog::write(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
